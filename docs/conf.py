07dd46f8bb4f999cb7110f41e370cb75cf170cbf
SDomazet <36760553+SDomazet@users.noreply.github.com>
2018-09-27 10:51:19 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -43,8 +43,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Read the Docs Template'
-copyright = u'2014, Read the Docs'
+project = u'Performa 365'
+copyright = u'2017, Performa 365'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
